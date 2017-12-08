ef05c86330b63cbdc0477d564eae61196cb5f62c
aeiou335 <b02703036@ntu.edu.tw>
2017-12-08 23:36:50 +0800

    finally finished

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+wget https://www.dropbox.com/s/av8kwvm7zyrdpma/ver2.3_GRU.h5
+wget https://www.dropbox.com/s/jbvgspcsmywa9ee/ver2.4_GRU.h5
+wget https://www.dropbox.com/s/wddllag0rn9yuym/nofunwithword2vec.bin
 python3 combine.py $1 $2 ver2.3_GRU.h5 ver2.4_GRU.h5 tokenizer.pickle
--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget https://www.dropbox.com/s/wddllag0rn9yuym/nofunwithword2vec.bin
 python3 train.py $1 $2 nofunwithword2vec.bin tokenizer.pickle
